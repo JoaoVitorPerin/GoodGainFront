ef077fd79010dbc54a7596d771aaf5730395674a
JoaoVitorPerin <joao.vperin@outlook.com>
2024-06-06 00:32:03 -0300

    feat(dashboard): :sparkles: alterado graficos para o echarts

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -2,15 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ChartModule } from 'primeng/chart';
 import { TokenService } from 'src/app/core/services/token.service';
 import { DashboardService } from './dashboard.service';
+import { toLocaleFixed } from 'src/app/core/ts/util';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    ChartModule
+    ChartModule,
+    NgxEchartsDirective
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
+  providers: [
+    provideEcharts(),
+  ]
 })
 export class DashboardComponent implements OnInit{
   nomeUser: any;
@@ -20,140 +27,112 @@ export class DashboardComponent implements OnInit{
   basicOptions: any;
   tipoAposta: any;
 
+  dadosCards: any;
+  chartCampeonatoOption: EChartsOption;
+  chartTipoOption: EChartsOption;
+  toLocaleFixed = toLocaleFixed;
+
   constructor(
     private tokenService: TokenService,
     private dashboardService: DashboardService
   ){}
 
   ngOnInit() {
-      const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--text-color');
-      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-      this.dataCampeonatos = {
-          labels: ['Campeonato'],
-          datasets: [
-            {
-              label: 'Premier League',
-              data: [56],
-              backgroundColor: [documentStyle.getPropertyValue('--purple-500')],
-              borderColor: [documentStyle.getPropertyValue('--purple-500')],
-              borderWidth: 1
-            },
-            {
-                label: 'Campeonato Brasileiro',
-                data: [34],
-                backgroundColor: [documentStyle.getPropertyValue('--blue-500')],
-                borderColor: [documentStyle.getPropertyValue('--blue-500')],
-                borderWidth: 1
-            },
-            {
-              label: 'Copa do Brasil',
-              data: [12],
-              backgroundColor: [documentStyle.getPropertyValue('--green-500')],
-              borderColor: [documentStyle.getPropertyValue('--green-500')],
-              borderWidth: 1
-            },
-          ]
-      };
-
-      this.tipoAposta = {
-        labels: ['Tipo de Aposta'],
-        datasets: [
-          {
-            label: '+8.5 escanteios',
-            data: [65],
-            backgroundColor: [documentStyle.getPropertyValue('--purple-500')],
-            borderColor: [documentStyle.getPropertyValue('--purple-500')],
-            borderWidth: 1
-          },
-          {
-            label: 'Ambos marcam',
-            data: [49],
-            backgroundColor: [documentStyle.getPropertyValue('--blue-500')],
-            borderColor: [documentStyle.getPropertyValue('--blue-500')],
-            borderWidth: 1
-          },
-          {
-            label: '+1.5 cartões amarelos',
-            data: [43],
-            backgroundColor: [documentStyle.getPropertyValue('--green-500')],
-            borderColor: [documentStyle.getPropertyValue('--green-500')],
-            borderWidth: 1
-          },
-          {
-            label: '+2.5 gols',
-            data: [32],
-            backgroundColor: [documentStyle.getPropertyValue('--yellow-500')],
-            borderColor: [documentStyle.getPropertyValue('--yellow-500')],
-            borderWidth: 1
-          },
-          {
-            label: '+1.5 cartões vermelhos',
-            data: [21],
-            backgroundColor: [documentStyle.getPropertyValue('--orange-500')],
-            borderColor: [documentStyle.getPropertyValue('--orange-500')],
-            borderWidth: 1
-          },
-          {
-            label: 'Vencedor',
-            data: [12],
-            backgroundColor: [documentStyle.getPropertyValue('--red-500')],
-            borderColor: [documentStyle.getPropertyValue('--red-500')],
-            borderWidth: 1
-          },
-        ]
-    };
-
-      this.basicOptions = {
-          plugins: {
-              legend: {
-                  labels: {
-                      color: textColor
-                  }
-              }
-          },
-          scales: {
-              y: {
-                  beginAtZero: true,
-                  ticks: {
-                      color: textColorSecondary
-                  },
-                  grid: {
-                      color: surfaceBorder,
-                      drawBorder: false
-                  }
-              },
-              x: {
-                  ticks: {
-                      color: textColorSecondary
-                  },
-                  grid: {
-                      color: surfaceBorder,
-                      drawBorder: false
-                  }
-              }
-          }
-      };
-
       this.buscarInfosPerfil();
       this.buscarDadosDashboard();
+
+      this.chartCampeonatoOption = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+              type: 'cross',
+              crossStyle: {
+                  color: '#999'
+              }
+          }
+        },
+        xAxis: [{
+          type: 'category',
+          data: [],
+        }],
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            data: [],
+            type: 'bar',
+            color: '#05FF00',
+          },
+        ],
+      };
+
+      this.chartTipoOption = {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+              type: 'cross',
+              crossStyle: {
+                  color: '#999'
+              }
+          }
+        },
+        xAxis: [{
+          type: 'category',
+          data: [],
+        }],
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            data: [],
+            type: 'bar',
+            color: '#05FF00',
+          },
+        ],
+      };
   }
 
   buscarInfosPerfil(){
     this.nomeUser = `${this.tokenService.getJwtDecoded().cli_info.nome} ${this.tokenService.getJwtDecoded().cli_info.sobrenome}`;
-    this.cpfUser = this.tokenService.getJwtDecoded().cli_info.cpf
+    this.cpfUser = this.tokenService.getJwtDecoded().cli_info.cpf;
   }
 
   buscarDadosDashboard(){
+
     this.dashboardService.buscarDados(this.cpfUser).subscribe({
-      next: (dados) => {
-        console.log(dados)
+      next: (res) => {
+        this.dadosCards = {
+          odd_media: res.dados.media_odds,
+          qtd_apostas: res.dados.qtd_apostas,
+          valor_medio: res.dados.valor_apostado,
+          tipoAposta: this.returnTipoAposta(res.dados.tipo_aposta_mais_escolhida, res.tipos)
+        };
+
+        this.chartCampeonatoOption.xAxis[0].data = res.dados.grafico_campeonatos.map((item: any) => this.returnCampeonato(item.campeonato, res.campeonatos));
+
+        this.chartCampeonatoOption.series[0].data = res.dados.grafico_campeonatos.map((item: any) => item.valor);
+
+        this.chartTipoOption.xAxis[0].data = res.dados.grafico_tipo_aposta.map((item: any) => this.returnTipoAposta(item.tipo_aposta, res.tipos));
+
+        this.chartTipoOption.series[0].data = res.dados.grafico_tipo_aposta.map((item: any) => item.valor);
       },
       error: (error) => {
         console.error(error);
       }
     });
+  }
+
+  returnTipoAposta(tipo: string, listaTipo: any){
+    const tipoAposta = listaTipo.find((item: any) => item.id === parseInt(tipo));
+    return tipoAposta.informacao;
+  }
+
+  returnCampeonato(campeonato: any, listaCampeonato: any){
+    console.log(campeonato, listaCampeonato);
+    const nomeCampeonato = listaCampeonato.find((item: any) => item.id === campeonato);
+    console.log(nomeCampeonato);
+    return nomeCampeonato.nome;
   }
 }
