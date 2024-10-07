4c8db766f45547cab2a4351467c16c94a9811868
João Vitor Perin <joao.perin@inteliger.com.br>
2024-10-07 18:50:56 -0300

    fix(gerais): :bug: implementado o docker

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -5,7 +5,7 @@ export const environment = {
   WEBSOCKET_ATIVO: false,
   TEMPO_ARMAZENAMENTO_SESSAO: 720,
   API_WS: '',
-  API_BACK: `https://goodgainapi.com.br/`,
-  API_ONLINE: 'https://goodgainapi.com.br/',
+  API_BACK: `http://localhost:8001/`,
+  API_ONLINE: 'http://localhost:8001/',
   VERSAO: appVersion
 };
